c3d0f313e8986e1b68fe7c724e26470f006f01c8
Lorién <lorienportella@gmail.com>
2021-04-30 19:36:09 +0200

    data visualization started

---
+++ Assets/DataViewer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class DataViewer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int lineCount = 100;
+    public float radius = 3.0f;
+
+    Camera camera;
+    RenderTexture texture;
+
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+}
+
+[CustomEditor(typeof(DataViewer))]
+public class DataViewerEditor : Editor
+{
+
+}
